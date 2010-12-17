a61f6ad037a5d93dc5453f8bbadafb754fbdb19f
Brian Hosie <brianhosie@gmail.com>
2010-12-16 18:12:22 -0700

    removed .gitignore -for now...

--- lib/sqlstatements.php
+++ lib/sqlstatements.php
@@ -1,10 +1,38 @@
 <?php
 
+//Reporting: Get the num of calls
 $getcalls = ("SELECT COUNT(contact_type)
 			FROM details
 			WHERE contact_type = ph
 			AND contact_date LIKE '2010-%';") //Need to fix the date placeholder
 
 
+//Insert New Event
+$newEvent = ("INSERT INTO details (
+			contact_date,
+			contact_type,
+			num_attend,
+			in_out,
+			notes)
+			VALUES(
+			$_POST['myfield']
+			$_POST['myfield']
+			$_POST['myfield']
+			$_POST['myfield']
+			$_POST['myfield']);")
+
+//Insert New Contact
+$newContact = ("INSERT INTO contacts(
+				firstname,
+				lastname,
+				phone,
+				email,
+				title,
+				school_code,
+				alt_school_code,
+				lead);")
+
+
+			
 
 ?>

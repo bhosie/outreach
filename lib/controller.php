fe96dacb429b1de687161cc5299c56bc972b92b6
Christer Edwards <christer.edwards@gmail.com>
2010-12-16 19:31:25 -0700

    added var_dump to controller (testing)

--- lib/controller.php
+++ lib/controller.php
@@ -1,13 +1,12 @@
 <?php
 
-
   	echo "Date: {$_POST['date']}<br />";
     echo "Inquiry: {$_POST['inquiry']}<br />";
     echo "Attended: {$_POST['attendance']}<br /><br />";
 	echo "Contact Type: {$_POST['radio']}<br /><br />";
 	echo "Notes: {$_POST['notes']}<br /><br />";
-
-
+    
+    var_dump($_POST);
 
 ?>
 

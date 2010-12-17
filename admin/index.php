c95f347167b0f891cac3da131f1981a4fb05be3e
Christer Edwards <christer.edwards@gmail.com>
2010-12-16 19:43:58 -0700

    fixed tabindex and added username/password on admin

--- admin/index.php
+++ admin/index.php
@@ -11,62 +11,51 @@ include($_SERVER['DOCUMENT_ROOT'] . '/outreach/includes/html/sidebar.html.php');
 
 ?>
 		
-		<div id="content">
+	<div id="content">
 		
-			<form action="controller.php" method="post" class="f-wrap-1">
-			
-			<div class="req"><b>*</b> Indicates required field</div>
-			
-			<fieldset>
-			
-			<h3>User Management</h3>
-			
-			<label for="firstname"><b><span class="req">*</span>First name:</b>
-			<input id="firstname" name="firstname" type="text" class="f-name" tabindex="1" /><br />
-			</label>
-			
-			<label for="lastname"><b><span class="req">*</span>Last name:</b>
-			<input id="lastname" name="lastname" type="text" class="f-name" tabindex="2" /><br />
-			</label>
-			
-			<label for="emailaddress"><b><span class="req">*</span>Email Address:</b>
-			<input id="emailaddress" name="emailaddress" type="text" class="f-email" tabindex="3" /><br />
-			</label>
-			
-			<label for="enquiry"><b>User Role:</b>
-			<select id="enquiry" name="role" tabindex="4">
-			<option>Select...</option>
-			<option>Call Center</option>
-			<option>Team Leader</option>
-			<option>Manager</option>
-			</select>
-			<br />
-			</label>
-			
-			<fieldset class="f-radio-wrap">
+		<form action="controller.php" method="post" class="f-wrap-1">
 		
-			<b>Country:</b>
+		<div class="req"><b>*</b> Indicates required field</div>
 			
-				<fieldset>
-				
-				<label for="australia">
-				<input id="australia" type="radio" name="radio" value="Australia" class="f-radio" tabindex="8" />
-				Australia</label>
-				
-				<label for="newzealand">
-				<input id="newzealand" type="radio" name="radio" value="New Zealand" class="f-radio" tabindex="9" />
-				New Zealand</label>
-				
-				<label for="antarctica">
-				<input id="antarctica" type="radio" name="radio" value="Antarctica" class="f-radio" tabindex="10" />
-				Antarctica</label>
-	
-				</fieldset>
+	<fieldset>
 			
-			</fieldset>
-            <hr/>
-            <input type="submit" name="user" value="Create User" class="f-submit" tabindex="11" />
-            </form>
+		<h3>User Management</h3>
+			
+		<label for="firstname"><b><span class="req">*</span>First name:</b>
+		<input id="firstname" name="firstname" type="text" class="f-name" tabindex="1" /><br />
+		</label>
+			
+		<label for="lastname"><b><span class="req">*</span>Last name:</b>
+		<input id="lastname" name="lastname" type="text" class="f-name" tabindex="2" /><br />
+		</label>
+			
+		<label for="emailaddress"><b><span class="req">*</span>Email Address:</b>
+		<input id="emailaddress" name="emailaddress" type="text" class="f-email" tabindex="3" /><br />
+		</label>
+			
+		<label for="enquiry"><b><span class="req">*</span>User Role:</b>
+		<select id="enquiry" name="role" tabindex="4">
+		<option>Select...</option>
+		<option>Call Center</option>
+		<option>Team Leader</option>
+		<option>Manager</option>
+		</select>
+		<br />
+		</label>
+
+		<label for="username"><b><span class="req">*</span>Username:</b>
+		<input id="username" name="username" type="text" class="f-name" tabindex="5" /><br />
+		</label>
+			
+		<label for="password"><b><span class="req">*</span>Password</b>
+		<input id="password" name="password" type="password" class="f-name" tabindex="6" /><br />
+		</label>
+			
+	</fieldset>
+        
+    <hr/>
+    <input type="submit" name="user" value="Create User" class="f-submit" tabindex="7" />
+    </form>
 
 <?php
 

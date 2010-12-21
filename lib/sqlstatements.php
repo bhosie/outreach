393a1a54dd08141f3e5f28632e9ea4b6dddd3282
Brian Hosie <brianhosie@gmail.com>
2010-12-20 17:52:04 -0700

    updated sqlstatements file

--- lib/sqlstatements.php
+++ lib/sqlstatements.php
@@ -11,7 +11,7 @@
 $getcalls = ("SELECT COUNT(contact_type)
 			FROM details
 			WHERE contact_type = ph
-			AND contact_date LIKE '2010-%';"); //Need to fix the date placeholder
+			AND contact_date LIKE '2010-%';");	 //Need to fix the date placeholder
 
 /*************************************************************************************
 *************************************************************************************/

081795034fc0c79c1a89d684a7d69d531b6e8072
Brian Hosie <brianhosie@gmail.com>
2010-12-22 10:25:12 -0700

    updated sqlstatements file

--- lib/sqlstatements.php
+++ lib/sqlstatements.php
@@ -13,68 +13,6 @@ $getcalls = ("SELECT COUNT(contact_type)
 			WHERE contact_type = ph
 			AND contact_date LIKE '2010-%';");	 //Need to fix the date placeholder
 
-/*************************************************************************************
-*************************************************************************************/
 
-///////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////////////////////////////////////////////////////////////
-
-/*********************************************************************************
-************************** INSERT FUNCTIONS ***********************************/
-
-/*$newEvent = ("INSERT INTO details (
-					contact_date,
-					contact_type,
-					num_attend,
-					in_out,
-					notes)
-					VALUES(
-					'{$_POST['date']}',
-					'{$_POST['inqury']}',
-					'{$_POST['attendance']}',
-					'{$_POST['contact-type']}',
-					'{$_POST['notes']}');");
-	
-
-$newContact = ("INSERT INTO contacts(
-						firstname,
-						lastname,
-						phone,
-						email,
-						title,
-						school_code,
-						alt_school_code,
-						lead)
-						VALUES(
-						'{$_POST['firstname']}',
-						'{$_POST['lastname']}',
-						'{$_POST['phone']}',
-						'{$_POST['emailaddress']}',
-						'{$_POST['jobtitle']}',
-						'{$_POST['schoolcode']}',
-						'{$_POST['altschoolcode']}',
-						'{$_POST['lead']}');");
-
-$newUser = ("INSERT INTO users(
-					firstname,
-					lastname,
-					email,
-					user_role,
-					username,
-					password)
-					VALUES(
-					'{$_POST['firstname']}',
-					'{$_POST['lastname']}',
-					'{$_POST['emailaddress']}',
-					'{$_POST['role']}',
-					'{$_POST['username']}',
-					md5('{$_POST['password']}'));");
-	
-
-*/
-/*************************************************************************************
-*************************************************************************************/
-			
 
 ?>

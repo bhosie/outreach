5fbfdea4cdad1176fadbc44ee11a79bc386faa87
Brian Hosie <brianhosie@gmail.com>
2010-12-23 11:47:35 -0700

    checking user_role in sessions

--- lib/sessions.php
+++ lib/sessions.php
@@ -8,11 +8,14 @@
 						WHERE username = '{$_POST['username']}'
 						AND password = md5('{$_POST['password']}');";
 
-	echo 'This is $authentication: ' . $authentication;
+	$role = "SELECT user_role FROM users
+				WHERE username = '{$_POST['username']}';";
+
 						
 	$result = mysqli_query($connect, $authentication);
+	$roleresult = mysqli_query($connect, $role);
 	
-	if(!$result) {
+	if(!$result || !$roleresult) {
 		$error = "Query error: " . mysqli_error($connect);
 		
 		include($_SERVER['DOCUMENT_ROOT'] . '/outreach/includes/html/loginerror.html.php');
@@ -21,10 +24,24 @@
 	}
 	
 	$rowcount = mysqli_num_rows($result);
+
+	while ($row = mysqli_fetch_assoc($roleresult)) {
+     $userrole = $row['user_role'];
+}	
 	
 	if($rowcount == 1){
+		
+		//convert USER ROLE to string
+		//$userrole = mysqli_result($roleresult);
+		//If user exists, grab user role
+		
+		
 		$_SESSION['username'] = $_POST['username'];
-		header('Location: /outreach/home/');
+		$_SESSION['user_role'] = $userrole;
+		echo $_SESSION['username'];
+		echo $_SESSION['user_role'];
+ 
+		//header('Location: /outreach/home/');
 	}else {
 		include($_SERVER['DOCUMENT_ROOT'] . '/outreach/includes/html/loginerror.html.php');
 	}

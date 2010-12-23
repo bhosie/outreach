fadfd7eeaf0e793862c570a978486487ca949e0a
Brian Hosie <brianhosie@gmail.com>
2010-12-23 10:29:12 -0700

    nonavigation.php

---
+++ lib/nonavigation.php
@@ -0,0 +1,13 @@
+<?php
+//
+//THIS FILE IS USED TO VERIFY THAT A FORM WAS SUBMITTED TO ACCESS THE CONTROLLER AND PREVENTS
+//DIRECT ACCESS TO THE CONTROLLER FILE(S).
+//
+if(isset($_POST['event']) || isset($_POST['contact']) ||
+	isset($_POST['user']) || isset($_POST['searchsite']) || isset($_POST['searchcontact'])){
+		//do nothing, continue with script
+}else{ 
+	//DIRECT NAVIGATION TO RISTRICTED PAGE! Redirect to home.
+	header('Location: /outreach/home');
+}
+?>

b554c256cf8d9f2170fac5e708f95a5e40cde4aa
Brian Hosie <brianhosie@gmail.com>
2010-12-23 14:01:39 -0700

    updated nonav

--- lib/nonavigation.php
+++ lib/nonavigation.php
@@ -4,7 +4,7 @@
 //DIRECT ACCESS TO THE CONTROLLER FILE(S).
 //
 if(isset($_POST['event']) || isset($_POST['contact']) ||
-	isset($_POST['user']) || isset($_POST['searchsite']) || isset($_POST['searchcontact'])){
+	isset($_POST['user']) || isset($_POST['searchsite']) || isset($_POST['searchcontact']) || isset($results)){
 		//do nothing, continue with script
 }else{ 
 	//DIRECT NAVIGATION TO RISTRICTED PAGE! Redirect to home.

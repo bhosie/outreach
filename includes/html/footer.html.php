78102e50e65d39d09e879a803e660cfb73ad2430
Brian Hosie <brianhosie@gmail.com>
2011-01-08 16:33:17 -0700

    added change password link to footer

--- includes/html/footer.html.php
+++ includes/html/footer.html.php
@@ -1,6 +1,8 @@
 <div id="footer">
 			<p>&copy; UHEAA <?php echo date("Y"); ?></p>
-			<p><a href="#">Contact Us</a> | <a href="#">Privacy</a> | <a href="#">Links</a></p>
+			<p>
+			<a href="#">Contact Us</a> | <a href="#">Privacy</a> | <a href="#">Links</a> <?php if(isset($_SESSION)){echo " | <a href ='/outreach/password/'>Change Password</a>";} ?>
+			</p>
 			</div>
 
 		</div>

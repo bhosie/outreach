15e69a4d880b1014f178deb1b18fd3b925b3ff1b
Brian Hosie <brianhosie@gmail.com>
2011-01-10 10:35:21 -0700

    adding comments and improved logic on eventcontroller

--- lib/eventcontroller.php
+++ lib/eventcontroller.php
@@ -18,7 +18,8 @@ include($_SERVER['DOCUMENT_ROOT'] . '/outreach/includes/html/sidebar.html.php');
 	if(isset($_POST['event'])){
 		
 			$date = ($_POST['date']);
-			 					
+			
+			//Verify all required fields are completed 					
 			if (empty($_POST['date']) || empty($_POST['notes']) || empty($_POST['in_out'])){
 
 				$error = "Please enter all the * required information";
@@ -27,6 +28,8 @@ include($_SERVER['DOCUMENT_ROOT'] . '/outreach/includes/html/sidebar.html.php');
 
 			}
 			
+
+			//verify email matches valid criteria
 			if (!preg_match ('/^[0-9]{4}-[0-1]{1}[0-9]{1}-[0-3]{1}[0-9]{1}$/', $date)) {
 			                  
 				$FormValid="False";
@@ -37,7 +40,7 @@ include($_SERVER['DOCUMENT_ROOT'] . '/outreach/includes/html/sidebar.html.php');
 			}
 			
 			
-			
+			//If above criteria checks out, insert new event into DB
 			$newEvent = ("INSERT INTO details (
 					user_id,
 					contact_id,
@@ -57,6 +60,16 @@ include($_SERVER['DOCUMENT_ROOT'] . '/outreach/includes/html/sidebar.html.php');
 
 			$result = mysqli_query($connect, $newEvent);
 			
+			//Throw error if not successfully added
+			if(!$result) {
+				$error = "Query error: " . mysqli_error($connect);
+				
+				include($_SERVER['DOCUMENT_ROOT'] . '/outreach/includes/html/error.html.php');
+				exit();
+			}
+
+
+			//Set variables for displaying details about newly added event
 			$success = 	
 			(" <table width='200' border='0' cellspacing='0' cellpadding='0' align='left'>
 			  <tr>
@@ -82,16 +95,11 @@ include($_SERVER['DOCUMENT_ROOT'] . '/outreach/includes/html/sidebar.html.php');
 			</table>");
 			
 			
-			
 
 			require($_SERVER['DOCUMENT_ROOT'] . '/outreach/includes/html/success.html.php');
-			
-		if(!$result) {
-				$error = "Query error: " . mysqli_error($connect);
-				
-				include($_SERVER['DOCUMENT_ROOT'] . '/outreach/includes/html/error.html.php');
-				exit();
-			}
+		
+		
+		
 			
 	}
 
